921b101202924e9c193cb5a327e2748c8c330547
liuchunming <liuchunming@liuchunmings-MacBook-Pro.local>
2015-11-17 15:14:13 +0800

    rename jenkins-slave to jenkins-slave.conf in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-cp jenkins-slave /etc/sysconfig/jenkins-slave
+cp jenkins-slave.conf /etc/sysconfig/jenkins-slave
 cp jenkins-slave.init /etc/init.d/jenkins-slave
 chmod 770 /etc/init.d/jenkins-slave
 chkconfig --level 3 jenkins-slave on
