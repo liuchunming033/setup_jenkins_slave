a876f69b8b8c852fc1f6fce15972c1188d6e7c1f
liuchunming1 <liuchunming1@conew.com>
2016-01-18 11:40:15 +0800

    add cron task to monitor slave

--- jenkins-monitor.sh
+++ jenkins-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:~/bin
+export PATH
 c=$(ps -ef | grep "java -jar slave.jar"|grep -v grep | wc -l)
-if [ $c -eq 0 ]
-then
+if [ $c -eq 0 ];then
   service jenkins-slave restart
 fi
-
