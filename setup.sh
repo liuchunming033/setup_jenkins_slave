6f69d6362a33e7a1623c3c18bb5b2e29a3630573
liuchunming1 <liuchunming1@conew.com>
2016-01-21 17:18:17 +0800

    auto restart jenkins slave service if it is down

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ cp jenkins-slave.init /etc/init.d/jenkins-slave
 chmod 770 /etc/init.d/jenkins-slave
 chkconfig --level 3 jenkins-slave on
 service jenkins-slave restart
+chmod 644 monitor-jenkins
+cp monitor-jenkins /etc/cron.d/
+service crond restart
